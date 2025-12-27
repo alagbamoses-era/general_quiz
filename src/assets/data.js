7bb9cf625f0d4d07656a9288284f720934dc6ac8
Moses <agunbiademoses2009@gmail.com>
2025-12-27 20:01:04 +0000

    added data.js (questions)

---
+++ src/assets/data.js
@@ -0,0 +1,35 @@
+data =
+[
+{
+question: When Mary became pregnant, Mary and Joseph were: 
+option1: married
+option2: engaged
+option3: just friends
+option4: none of the above
+ans: 2
+},
+{
+question: When Mary became pregnant
+option1: Joseph married her 
+option2: Joseph wanted to dissolve their relationship
+option3: Mary left Nazareth for a while 
+option4: an angel told them to go to Bethlehem
+ans: 2, 3, 4
+},
+{
+question: Who directed Mary and Joseph to go to Bethlehem?
+option1: Herod 
+option2: Caesar 
+option3: an angel 
+option4: the IRS
+ans: 2 
+},
+{
+question: Joseph’s family was originally from
+option1: Jerusalem 
+option2: Bethlehem 
+option3: Nazareth 
+option4: none of the above
+Answer: 2 
+}
+]
